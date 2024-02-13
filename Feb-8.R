1376e0b5e9c7c6d239209c1a1455968ba911a34e
Neill Miller <156738820+NeiMiller04@users.noreply.github.com>
2024-02-13 10:22:30 -0500

    Add files via upload

--- Feb-8.R
+++ Feb-8.R
@@ -1,9 +1,14 @@
 Author:Miller,Date:02/08/2024,Purpose:Calculate the correlation test
 
+
 #Load the library ggpubr 
 library(ggpubr)
 
 # Load some dummy dataset 
 my_data <- mtcars 
 
-#ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+#ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", cor.method = "pearson", xlab = "Fuel Consumption", ylab = "Car Weight")
+
+#Calculating the correlation test value 
+res<-cor.test(my_data$wt,my_data$mpg,method="pearson")
+
